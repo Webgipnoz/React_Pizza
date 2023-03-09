5f72b27514650ca7eb1c37f29bf88935ddd75b6e
Slav Lomachynkyi <slavchek.3777@gmail.com>
2023-03-10 02:29:21 +0300

    add onClickCategories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,50 @@
+import React from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onclickCategory = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => onclickCategory(0)}
+          className={activeIndex === 0 ? "active" : ""}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => onclickCategory(1)}
+          className={activeIndex === 1 ? "active" : ""}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => onclickCategory(2)}
+          className={activeIndex === 2 ? "active" : ""}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => onclickCategory(3)}
+          className={activeIndex === 3 ? "active" : ""}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={() => onclickCategory(4)}
+          className={activeIndex === 4 ? "active" : ""}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => onclickCategory(5)}
+          className={activeIndex === 5 ? "active" : ""}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
