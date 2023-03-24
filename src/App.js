7d231cfa2a2b11249f18a37d43429a24e4c4acfc
Slav Lomachynkyi <slavchek.3777@gmail.com>
2023-03-24 04:37:53 +0300

    add pages and homePage

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Header from "./components/Header";
-import Sort from "./components/Sort";
-import Categories from "./components/Categories";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton.jsx";
+import Home from "./Pages/Home";
 
 import "./scss/app.scss";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://641a4369f398d7d95d53c155.mockapi.io/items")
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
